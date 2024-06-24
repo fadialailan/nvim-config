009c7219d7af40f4bc8488bcb18b3d9db883ce0f
Fadi Alailan <fadialailane@yahoo.com>
2024-06-24 09:21:52 -0400

    fixed bugs due to breaking changes with the trouble plugin

--- lua/user/trouble.lua
+++ lua/user/trouble.lua
@@ -4,11 +4,11 @@ return {
 		local trouble = require("trouble")
 		trouble.setup({})
 		vim.keymap.set("n", "<leader>xx", function() trouble.open() end, {desc ="open"})
-		vim.keymap.set("n", "<leader>xw", function() trouble.open("workspace_diagnostics") end, {desc ="workspace"})
-		vim.keymap.set("n", "<leader>xd", function() trouble.open("document_diagnostics") end, {desc ="document"})
+		vim.keymap.set("n", "<leader>xd", function() trouble.open("symbols") end, {desc ="document symbols"})
+		vim.keymap.set("n", "<leader>xw", function() trouble.open("diagnostics") end, {desc ="workspace"})
 		vim.keymap.set("n", "<leader>xq", function() trouble.open("quickfix") end, {desc ="quickfix"})
 		vim.keymap.set("n", "<leader>xl", function() trouble.open("loclist") end, {desc ="loclist"})
 		vim.keymap.set("n", "<leader>xr", function() trouble.open("lsp_references") end, {desc ="lsp ref"})
-		vim.keymap.set("n", "<leader>xt", "<cmd>TodoTrouble<cr>", {desc ="todo"})
+		vim.keymap.set("n", "<leader>xt", function() trouble.open("todo") end, {desc ="todo"})
 	end
 }
