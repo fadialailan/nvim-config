a53957d01bd0bbd3b99f29821bc591d6e0904118
Fadi Alailan <fadialailane@yahoo.com>
2024-10-27 07:48:46 -0400

    performed changes to some ftplugin files

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -1,5 +1,15 @@
+--- code for detecting if other ftplugin has been loaded
+
+-- local did_ftplugin_number = vim.fn.exists("b:did_ftplugin")
+-- if did_ftplugin_number == 1 then
+-- 	return
+-- end
+
+-- disable default plugin
+vim.b.did_ftplugin = 1
+
 
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 0
---
+++ ftplugin/yaml.lua
@@ -0,0 +1,10 @@
+-- disable default plugin
+-- vim.b.did_ftplugin = 1
+
+-- disable yaml_recommended_style
+vim.g.yaml_recommended_style = 0
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
