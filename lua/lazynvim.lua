25a7bc150b70d47f0c533ca56281ccd7eeeccd64
Fadi Alailan <fadialailane@yahoo.com>
2024-12-28 12:03:40 -0500

    added some plugins

--- lua/lazynvim.lua
+++ lua/lazynvim.lua
@@ -13,15 +13,64 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
-	{"isobit/vim-caddyfile"},
-	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	{ "folke/neodev.nvim",  config = require("user/neodev") },
+	{ "isobit/vim-caddyfile" },
+	{ "folke/neoconf.nvim",  cmd = "Neoconf" },
+	{ "folke/neodev.nvim",   config = require("user/neodev") },
 	{
 		"sainnhe/sonokai",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.cmd("colorscheme sonokai")
+		end
+	},
+	{
+		'jakewvincent/mkdnflow.nvim',
+		config = function()
+			require('mkdnflow').setup({
+				-- Config goes here; leave blank for defaults
+				perspective = {
+					priority = "root",
+					root_tell = "index.md"
+				},
+				mappings = {
+					MkdnEnter = { { 'n', 'v' }, '<CR>' },
+					MkdnTab = false,
+					MkdnSTab = false,
+					MkdnNextLink = { 'n', '<Tab>' },
+					MkdnPrevLink = { 'n', '<S-Tab>' },
+					MkdnNextHeading = { 'n', ']]' },
+					MkdnPrevHeading = { 'n', '[[' },
+					MkdnGoBack = { 'n', '<BS>' },
+					MkdnGoForward = { 'n', '<Del>' },
+					MkdnCreateLink = false,  -- see MkdnEnter
+					MkdnCreateLinkFromClipboard = { { 'n', 'v' }, '<leader>mp' }, -- see MkdnEnter
+					MkdnFollowLink = false,  -- see MkdnEnter
+					MkdnDestroyLink = { 'n', '<M-CR>' },
+					MkdnTagSpan = { 'v', '<M-CR>' },
+					MkdnMoveSource = { 'n', '<F2>' },
+					MkdnYankAnchorLink = { 'n', 'yaa' },
+					MkdnYankFileAnchorLink = { 'n', 'yfa' },
+					MkdnIncreaseHeading = { 'n', '+' },
+					MkdnDecreaseHeading = { 'n', '-' },
+					MkdnToggleToDo = { { 'n', 'v' }, '<C-Space>' },
+					MkdnNewListItem = false,
+					MkdnNewListItemBelowInsert = { 'n', 'o' },
+					MkdnNewListItemAboveInsert = { 'n', 'O' },
+					MkdnExtendList = false,
+					MkdnUpdateNumbering = { 'n', '<leader>mnn' },
+					MkdnTableNextCell = { 'i', '<Tab>' },
+					MkdnTablePrevCell = { 'i', '<S-Tab>' },
+					MkdnTableNextRow = false,
+					MkdnTablePrevRow = { 'i', '<M-CR>' },
+					MkdnTableNewRowBelow = { 'n', '<leader>mir' },
+					MkdnTableNewRowAbove = { 'n', '<leader>miR' },
+					MkdnTableNewColAfter = { 'n', '<leader>mic' },
+					MkdnTableNewColBefore = { 'n', '<leader>miC' },
+					MkdnFoldSection = { 'n', '<leader>mf' },
+					MkdnUnfoldSection = { 'n', '<leader>mF' }
+				}
+			})
 		end
 	},
 	{
@@ -40,7 +89,8 @@ require("lazy").setup({
 					lsp = true,
 				},
 			})
-			vim.keymap.set({ "n" }, "<leader>cct", "<cmd>CccHighlighterToggle<cr>", { desc = "toggle color display" })
+			vim.keymap.set({ "n" }, "<leader>cct", "<cmd>CccHighlighterToggle<cr>",
+				{ desc = "toggle color display" })
 		end
 	},
 	-- {
